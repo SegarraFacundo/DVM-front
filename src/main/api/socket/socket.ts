20044de782d1814914cbe3e519756a49bf8cc262
Facundo Segarra <segarrafacundoroman@gmail.com>
2024-02-12 13:32:12 -0300

    Version 1.0.7

--- src/main/api/socket/socket.ts
+++ src/main/api/socket/socket.ts
@@ -12,18 +12,29 @@ export interface DatosMeteorologicos {
   puntoDeRocio: number | null
 }
 
+const isJsonString = (value: string) => {
+  try {
+    JSON.parse(value)
+  } catch (e) {
+    return false
+  }
+  return true
+}
+
 export const asyncEmit = (command: 'datosMeteorologicos'): DatosMeteorologicos => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-expect-error
   return new Promise<DatosMeteorologicos>((resolve, reject) => {
     client.on('data', (data) => {
       const infoData = data?.toString('utf-8')
-      const infoDataJson = JSON.parse(infoData)
-      if (infoDataJson && infoDataJson.command === command) {
-        const datosMeteorologicos: DatosMeteorologicos = {
-          ...(infoDataJson as DatosMeteorologicos)
+      if (isJsonString(infoData)) {
+        const infoDataJson = JSON.parse(infoData)
+        if (infoDataJson && infoDataJson.command === command) {
+          const datosMeteorologicos: DatosMeteorologicos = {
+            ...(infoDataJson as DatosMeteorologicos)
+          }
+          resolve(datosMeteorologicos)
         }
-        resolve(datosMeteorologicos)
       }
     })
     setTimeout(reject, 1000)
